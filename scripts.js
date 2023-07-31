55764093012231d04e1e8ce90ec1db0eb15ac0f1
Samantha St-Germain <samantha.stgermain07@gmail.com>
2023-07-30 23:21:12 -0400

    added JS file and implemented typewriter effect

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const text = "a front end web developer";
+let charIndex = 0;
+const h3Element = document.getElementById("typewriter");
+
+function typeWriter() {
+    if (charIndex < text.length) {
+        h3Element.innerHTML += text.charAt(charIndex);
+        charIndex++;
+        setTimeout(typeWriter, 100);
+    }
+}
+
+typeWriter();
