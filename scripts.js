6e265c072c3103ae44ebb9731854fd7ed72ce0ea
Samantha St-Germain <samantha.stgermain07@gmail.com>
2023-11-13 17:03:56 -0500

    Updated CSS and JS to make titles appear under skills icons on mouseover

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// Typewriter effect
 const text = "front end web developer";
 let charIndex = 0;
 const h3Element = document.getElementById("typewriter");
@@ -11,3 +12,24 @@ function typeWriter() {
 }
 
 typeWriter();
+
+// Skills Titles
+document.addEventListener('DOMContentLoaded', function () {
+    const skillsIcons = document.querySelectorAll('.skillsIcons li');
+
+    skillsIcons.forEach((icon) => {
+        icon.addEventListener('mouseover', function () {
+            const title = document.createElement('span');
+            title.className = 'icon-title';
+            title.textConent = icon.getAttribute('data-title');
+            icon.appendChild(title);
+        });
+
+        icon.addEventListener('mouseout', function () {
+            const title = icon.querySelector('.icon-title');
+            if (title) {
+                icon.removeChild(title);
+            }
+        });
+    });
+});
